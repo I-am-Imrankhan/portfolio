1910f6173230cade3a9f1151a6093b5ef55ef64f
Imran Khan <imrankhanswati@gmail.com>
2022-03-11 03:46:16 +0100

    Adding project files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# portfolio
+Hanieh Davallou portfolio
---
+++ js/myresume-data.js
@@ -0,0 +1,239 @@
+function myresumeData() {
+  const experince = [
+    {
+      name: "Frontend",
+      type: "Frontend development - EMARKET",
+      purpose: "Hobby project",
+      about: "ReactJS app with Firebase as backend/auth.",
+      description: "This is my own/hobby project where i tried to use JavaScript as much as possible. It's a mini kind of blocket for buying and selling things.",
+      imageurl: "./img/web.png",
+      projectDetails: [
+        {
+          name: "eMarket",
+          url: "https://confident-euler-3480e1.netlify.app/",
+          icon: ""
+        },
+        {
+          icon: "",
+          githubUrl: "https://github.com/I-am-Imrankhan/eMarket",
+        },
+        {
+          keyPoints: [
+            "Firebase (Firestore, FirebaseAuth)",
+            "React, JavaScript, HTML, CSS, Styled Components, Bootstrap",
+          ],
+        },
+    ],
+    period: "Sept 2021 - Jan 2022" 
+    },
+    {
+        name: "ACTIC",
+        type: "web/mobile",
+        purpose: "frontend/app development",
+        about: "Modified both apps for android and ios and made new features for webb.",
+        description: `Actic is a leading wellness chain with more than 170 facilities and over 210,000 members in Sweden, Norway, Germany and Austria.
+        Sweden is the single largest market for Actic with approximately 170,000 members distributed over 120 training centers from Ystad in the south to Kiruna in the north. With the MAX card, you can practice on the majority of these.
+        As early as 1981, our first gym was started in Sweden. Then we were called Nautilus. Even today, we help people to a healthier life. Our focus is on creating a healthier society by inspiring and engaging all people to better health and increased well-being.",
+        `,
+        imageurl: "./img/web_mobile.png",
+        projectDetails: [
+          {
+            name: "",
+            url: "",
+            icon: ""
+          },
+          {
+            githubUrl: "",
+            icon: ""
+          },
+          {
+            keyPoints: [
+              "ios - adding new functionality to the MyActic app. Deploying and managing everything on playStore. Interface changes.",
+              "Actic (Wordpress website)",
+              "android - adding new functionality to the MyActic app. Deploying and managing",
+              "everything on playStore. Interface changes.",
+              "JavaScript, HTML, CSS",
+              "vsCode + Git",
+              "Slack, Trello and Teams, daily Scrum. Teams"
+            ],
+          },
+        ],
+        period: "18 nov 2020 - 18 may 2021" 
+      },
+
+      {
+        name: "AIDHEDGE",
+        type: "frontend/app",
+        purpose: "FRONTEND, STOCKHOLM ",
+        about: "Developed a website with Vue and made modification to ionic app",
+        description: "The AidHedge platform provides a continuous analysis of one or more projects, ensuring updated data on financial risk, transactions, budget, etc. The service also provides automatic reports in different formats (donor/organisation/audit) . Key functionality includes:",
+        imageurl: "./img/web_mobile.png",
+        projectDetails: [
+          {
+            name: "",
+            url: "",
+            icon: ""
+          },
+          {
+            githubUrl: "",
+            icon: ""
+          },
+          {
+            keyPoints: [
+              "Frontend",
+              "Ionic cross platform app development",
+              "Vue, SQL, JavaScript, HTML, CSS, npm, vsCode + Git, Slack + Trello",
+              "",
+            ],
+          },
+        ],
+        period: "11 mars 2019 - 04 aug 2019" 
+      },
+
+      {
+        name: "MINDFULHU ",
+        type: "app",
+        purpose: "IOS APP DEVELOPMENT/INTERN, STOCKHOLM",
+        about: "To create an ios application which can communicate with the phisical mindfulness device",
+        description: "Worked together with Stockholm and KTH students to make a ios mobile application to interact with the MindfulHU device. Mindfulness in everyday life from an eastern philosophic thinking. Regarding to the Tina Zhu(researcher Stocholm University) design practice provides a larger understanding and exploration on how to apply Chinese aesthetics to live with our body and nature in unison, harmony and appreciation.",
+        imageurl: "./img/mobile.png",
+        projectDetails: [
+          {
+            name: "",
+            url: "",
+            icon: ""
+          },
+          {
+            githubUrl: "",
+            icon: ""
+          },
+          {
+            keyPoints: [
+                "Brainstorming, BLE(Bluetooth) - IOS mobile app , xCode/Swift, Weekly",
+                "oll-scrum/Stand-ups, Weekly roll-scrum/Stand-up, Git, Agile, Scrum, WeChat"
+            ],
+          },
+        ],
+        period: "03 mars 2018 – 28 maj 2018" 
+      },
+      {
+        name: "ITANCAN",
+        type: "app",
+        purpose: "ANDROID APP DEVELOPMENT/INTERN, STOCKHOLM ",
+        about: "Worked with different android application, bug fixing and adding new functionality",
+        description: "Here i used to work on daily basis with both Android and ios native application. We had daily scrum standups everyday. This was my first internship in mobile app development.",
+        imageurl: "./img/mobile.png",
+        projectDetails: [
+          {
+            name: "",
+            url: "",
+            icon: ""
+          },
+          {
+            githubUrl: "",
+            icon: ""
+          },
+          {
+            keyPoints: [
+                "Android mobile development, Fixing bugs and implementing new functions",
+                "Team-work, Slack, Android Studio/Java",
+                "Scrum, Daily stand-up, Slack, Agile methods",
+                "Git" 
+            ],
+          },
+        ],
+        period: "15 oct 2017 – 28 dec 2017" 
+      },
+      {
+        name: "FLUTTER",
+        type: "app",
+        purpose: "Hobby projects",
+        about: "I love Flutter and the purpose to use it in my hobby projects was to learn it better",
+        description: "Well i love Flutter and i believe it will take over most part of mobile app development. I wanted to go into depth to learn it more. And thought why not to build something which can be fun and source of learning. ",
+        imageurl: "./img/mobile.png",
+        projectDetails: [
+          {
+            name: "Park&Pick and SalatTimes",
+            url: "https://play.google.com/console/u/0/developers/8380844659685341201/app-list?pli=1",
+            icon: ""
+          },
+          {
+            githubUrl: "",
+            icon: ""
+          },
+          {
+            keyPoints: [
+                "Flutter with dart",
+                "Android & IOS mobile applications",
+                "Github",
+                "vsCode",
+                "SQLLite",
+            ],
+          },
+        ],
+        period: "15 oct 2017 – 28 dec 2017" 
+      },
+      {
+        name: "EBUILDER",
+        type: "app",
+        purpose: "MOBILE-APP TESTER, KISTA STOCKHOLM",
+        about: "Testing mobile application functionality on different devices, finding bugs, reporting them to the team.",
+        description: "This company is in Kista, Stockholm. Quiet big company making mobile applications and web technologies. During the work we had daily standups and i was working with mobile applications functionalities. It was a source of learning for me to know the work place better.",
+        imageurl: "./img/mobile.png",
+        projectDetails: [
+          {
+            name: "",
+            url: "",
+            icon: ""
+          },
+          {
+            githubUrl: "",
+            icon: ""
+          },
+          {
+            keyPoints: [
+                "App-testing both for Android och IOS, Daily Stand-ups, Hybrid applikations",
+                "testing, Finding bugs. Microsoft Teams. Working with other team-members",
+            ],
+          },
+        ],
+        period: "Sept 2021 - Jan 2022" 
+      },
+  ];
+  let htmlCode = ``;
+  
+  experince.map((item, index) => {
+      htmlCode = htmlCode +
+        `
+        <div class="grid-item" id="btnPrepend">
+        <div id=${index}>
+        <img class="card-image" src=${item.imageurl} alt="type of development"/>
+        <h3 class="mb-0">${item.name}</h3>
+        <div class="subheading mb-3">${item.purpose}</div>
+        ${(item.projectDetails[0].url !== "" && item.name !== "FLUTTER")? `<a href=${item.projectDetails[0].url}> URL to live website: eMarket</a>`: ``}
+        ${(item.projectDetails[0].url !== "" && item.name === "FLUTTER")? `<a href=${item.projectDetails[0].url}> Google console</a>`: ``}
+        <br>
+        <span class="">${item.period}</span>
+        </p>
+        </div>
+        </div>
+        `;
+    });
+  const elephantCards = document.querySelector("section #experience");
+  
+  elephantCards.innerHTML = htmlCode;
+  $(document).on('click','#btnPrepend',function(e){
+      const listID = e.target.parentElement.id;
+      const litItem = experince[listID]
+      sessionStorage.setItem("experience-listItem", JSON.stringify(litItem));
+ 
+// This goes in the second web page:
+// Retrieve the sessionStorage variable
+// 
+  window.location.href = "./listItem.html"
+//
+
+  })
+  return experince;
+};
+myresumeData()
